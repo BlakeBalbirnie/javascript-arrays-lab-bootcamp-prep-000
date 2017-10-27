5929829dbd4d0d6f69dc6a73c35c2fd20b2d437f
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-10-27 16:50:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten (array, element) {
+function AppendKitten (array, element) {
   array = [...array, "Ralph"];
   return array;
 }
