6bbb1952e7b309133e251e158f35130f4f962fec
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-10-27 17:18:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function prependKitten(name) {
   return [name, ...kittens];
 }
 
-function removeLastKitten() {
-  return kittens.slice((0, kittens.length - 1);
+function removeLastKitten(name) {
+  return kittens.slice((0, kittens.length - 1)
 }
